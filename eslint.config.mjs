c986557433963bc39ef05783c87996ae06af11b6
Pedro <pedro.h.paula14@aluno.senai.br>
2026-08-25 20:12:22 -0300

    exercícios concluídos

---
+++ eslint.config.mjs
@@ -0,0 +1,67 @@
+/**
+ * THIS FILE WAS AUTO-GENERATED.
+ * PLEASE DO NOT EDIT IT MANUALLY.
+ * ===============================
+ * IF YOU COPY THIS INTO AN ESLINT CONFIG, REMOVE THIS COMMENT BLOCK.
+ */
+
+import path from 'node:path';
+
+import { includeIgnoreFile } from '@eslint/compat';
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import { configs, plugins } from 'eslint-config-airbnb-extended';
+import { rules as prettierConfigRules } from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+const gitignorePath = path.resolve('.', '.gitignore');
+
+const jsConfig = defineConfig([
+  // ESLint recommended config
+  {
+    name: 'js/config',
+    ...js.configs.recommended,
+  },
+  // Stylistic plugin
+  plugins.stylistic,
+  // Import X plugin
+  plugins.importX,
+  // Airbnb base recommended config
+  ...configs.base.recommended,
+]);
+
+const nodeConfig = defineConfig([
+  // Node plugin
+  plugins.node,
+  // Airbnb Node recommended config
+  ...configs.node.recommended,
+]);
+
+const prettierConfig = defineConfig([
+  // Prettier plugin
+  {
+    name: 'prettier/plugin/config',
+    plugins: {
+      prettier: prettierPlugin,
+    },
+  },
+  // Prettier config
+  {
+    name: 'prettier/config',
+    rules: {
+      ...prettierConfigRules,
+      'prettier/prettier': 'error',
+    },
+  },
+]);
+
+export default defineConfig([
+  // Ignore files and folders listed in .gitignore
+  includeIgnoreFile(gitignorePath),
+  // JavaScript config
+  ...jsConfig,
+  // Node config
+  ...nodeConfig,
+  // Prettier config
+  ...prettierConfig,
+]);
---
+++ index.js
@@ -0,0 +1,45 @@
+const cardapio = [
+    {nome: "Coca", preco: 6.5},
+    {nome: "Sorvete", preco: 10.0},
+    {nome: "Carbonara", preco: 16.0},
+    {nome: "Frango a Parmegiana", preco: 25.5},
+    {nome: "Bife a Milanesa", preco: 17.5}
+]
+
+const taxaEntrega = (km) => {
+    if(km <= 2) return 0;
+    if(km < 8) return 6;
+    return 6 + (km - 8) * 1.5;
+}
+
+const totalPedido = (valores) => {
+    let total = 0;
+
+    for (let i = 0; i < valores.length; i++) {
+        total += valores[i];
+    }
+    return total;
+}
+
+const buscarPreco = (cardapio, nome) => {
+    return cardapio.find((item) => item.nome === nome)?.preco || -1;
+}
+
+const teste = (titulo, esperado, retornado) => {
+  if (esperado === retornado) {
+    console.log(`${titulo} passou`);
+  } else {
+    console.error(`${titulo} não passou`);
+  }
+};
+
+// teste("1km", 0, taxaEntrega(1));
+// teste("2km", 0, taxaEntrega(2));
+// teste("7km", 6, taxaEntrega(7));
+// teste("10km", 9, taxaEntrega(9));
+// 
+// teste("pedido 1", 60, totalPedido([10, 20, 30]));
+// teste("pedido 2", 12, totalPedido([10, 20, 30]));
+// teste("pedido 3", 56, totalPedido([10, 20, 30]));
+
+console.log(buscarPreco(cardapio, "Coca-Cola"));
